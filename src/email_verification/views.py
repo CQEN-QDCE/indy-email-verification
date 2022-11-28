7b06e4c62bd5395916cb6ad7c103ee361edd2d03
Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>
2022-11-28 15:46:24 -0500

    [func] corrige methode verify_redirect
    Signed-off-by: Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>

--- src/email_verification/views.py
+++ src/email_verification/views.py
@@ -114,7 +114,9 @@ def in_progress(request, connection_id):
 
 
 def verify_redirect(request, connection_id):
+
     verification = get_object_or_404(Verification, connection_id=connection_id)
+    invitation_url = verification.invite_url
     didcomm_url = re.sub(r"^https?:\/\/\S*\?", "didcomm://invite?", invitation_url)
 
     template = loader.get_template("verify.html")
