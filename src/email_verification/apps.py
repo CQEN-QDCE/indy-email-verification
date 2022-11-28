4ccb83aa68d0a6a168fef9edec9d61f60cf29b5a
Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>
2022-11-28 11:39:01 -0500

    [func] corrige la version du schema
    Signed-off-by: Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>

--- src/email_verification/apps.py
+++ src/email_verification/apps.py
@@ -31,7 +31,7 @@ class EmailVerificationConfig(AppConfig):
         if cache.get("credential_definition_id") is None:
             schema_body = {
                 "schema_name": "CQEN_Droit_Acces_Virtuel",
-                "schema_version": "0.1",
+                "schema_version": "0.3",
                 "attributes": ["email", "time"]
             }
             schema_response = requests.post(f"{AGENT_URL}/schemas", headers={"x-api-key": API_KEY}, json=schema_body)
