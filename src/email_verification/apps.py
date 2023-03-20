afebe228cb146990c11beff98f070ec91084f68c
Philippe Foucault <philippe.foucault@mcn.gouv.qc.ca>
2023-03-20 15:45:10 -0400

    test print7
    Signed-off-by: Philippe Foucault <philippe.foucault@mcn.gouv.qc.ca>

--- src/email_verification/apps.py
+++ src/email_verification/apps.py
@@ -26,6 +26,9 @@ class EmailVerificationConfig(AppConfig):
 
         logger.info(f"adresse agent: {AGENT_URL}")
 
+# test phil
+        credential_definition_id = "FUKLxsjrYSHgScLbHuPTo4:3:CL:30685:RegistreAccesVirtuelCQEN"
+
         if cache.get("credential_definition_id") is None:
             schema_body = {
                 "schema_name": "CQENDroitAccesVirtuel",
@@ -37,29 +40,32 @@ class EmailVerificationConfig(AppConfig):
 
             logger.info(schema_response.text)
             print("**********************Goodbye 1A, cruel world!**********************")
-            print(schema_response.text)
+            #print(schema_response.text)
             print("**********************Goodbye 1F, cruel world!**********************")
 
-            schema_response_body = schema_response.json()
+            #schema_response_body = schema_response.json()
             print("**********************Goodbye 2, cruel world!**********************")
-            schema_id = schema_response_body["schema_id"]
+            # schema_id = schema_response_body["schema_id"]
+            schema_id = "FUKLxsjrYSHgScLbHuPTo4:2:CQENDroitAccesVirtuel:0.1.5"
+
             print("**********************Goodbye 3, cruel world!**********************")
 
-            credential_definition_body = {"schema_id": schema_id}
+            #credential_definition_body = {"schema_id": schema_id}
             print("**********************Goodbye 4, cruel world!**********************")
-            credential_definition_response = requests.post(
-                f"{AGENT_URL}/credential-definitions", headers={"x-api-key": API_KEY}, json=credential_definition_body
-            )
+            #credential_definition_response = requests.post(
+            #    f"{AGENT_URL}/credential-definitions", headers={"x-api-key": API_KEY}, json=credential_definition_body
+            #)
             print("**********************Goodbye 5, cruel world!**********************")
 
-            logger.info(credential_definition_response.text)
+            #logger.info(credential_definition_response.text)
             print("**********************Goodbye 6, cruel world!**********************")
 
-            credential_definition_response_body = credential_definition_response.json()
+            #credential_definition_response_body = credential_definition_response.json()
             print("**********************Goodbye 7, cruel world!**********************")
-            credential_definition_id = credential_definition_response_body[
-                "credential_definition_id"
-            ]
+            #credential_definition_id = credential_definition_response_body[
+            #    "credential_definition_id"
+            #]
+            credential_definition_id = "FUKLxsjrYSHgScLbHuPTo4:3:CL:30685:RegistreAccesVirtuelCQEN"
             print("**********************Goodbye 8, cruel world!**********************")
 
             logger.info(f"cred def id: {credential_definition_id}")
