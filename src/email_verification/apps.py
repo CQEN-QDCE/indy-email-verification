90c1aeaab67e5abea72c972fd29b66e4b4c93efe
Philippe Foucault <philippe.foucault@mcn.gouv.qc.ca>
2023-03-20 14:47:00 -0400

    increment no version 0.1.8
    Signed-off-by: Philippe Foucault <philippe.foucault@mcn.gouv.qc.ca>

--- src/email_verification/apps.py
+++ src/email_verification/apps.py
@@ -29,7 +29,7 @@ class EmailVerificationConfig(AppConfig):
         if cache.get("credential_definition_id") is None:
             schema_body = {
                 "schema_name": "CQENDroitAccesVirtuel",
-                "schema_version": "0.1.7",
+                "schema_version": "0.1.8",
                 "attributes": ["email", "time"]
             }
 
